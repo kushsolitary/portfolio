23bee47a228928eee5b2b39cd8f69a92329ffac8
Kushagra Agarwal <kushsolitary@gmail.com>
2015-08-22 20:19:38 +0530

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portfolio2015App
+ * @description
+ * # portfolio2015App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('portfolio2015App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
