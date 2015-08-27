685138b5397679fbb781e62d65d0f82547a3af5c
Kushagra Agarwal <kushsolitary@gmail.com>
2015-08-27 10:38:42 +0530

    add work gird

--- app/scripts/controllers/work.js
+++ app/scripts/controllers/work.js
@@ -8,10 +8,12 @@
  * Controller of the portfolioApp
  */
 angular.module('portfolioApp')
-  .controller('WorkCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('WorkCtrl', function ($scope, $http) {
+  	var token = 'e7b15fb9b16b80beeb965aa5ae9cc596292fe64dde6bc57fe8d5840698cac9bf';
+  	$scope.shots = []
+
+  	$http.get('https://api.dribbble.com/v1/users/kushsolitary/shots?access_token=' + token + '&per_page=8')
+  	.then(function(res) {
+  		$scope.shots = res.data;
+  	})
   });
